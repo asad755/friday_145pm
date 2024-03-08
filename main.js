acd37189881148305bf12b0230e7344526bc71fe
asad755 <143116178+asad755@users.noreply.github.com>
2024-03-08 13:52:36 +0500

    parcclass

---
+++ main.js
@@ -0,0 +1,16 @@
+// Array => [] => 0 , ....
+var students = ['Kehkashan', 'Hina', 'Sana', 'Saba', 'Iqra'];
+console.log(students);
+var firstStudentName = students[0];
+console.log(firstStudentName);
+console.log(students[1], students[2]);
+//For mix data type(string and numbers) in array
+var students_id = ['Iqra', 'Huma', 10, 15];
+console.log(students_id);
+//For Numbers and Boolean Dta Type in An Array
+var feesPaid = [true, false, 1000, 1500];
+console.log(feesPaid);
+var fruit_Name = ['APPLE', 'MANGO', 'ORANGE'];
+console.log(fruit_Name);
+fruit_Name[1] = 'GRAPES';
+console.log(fruit_Name);
---
+++ main.ts
@@ -0,0 +1,28 @@
+// Array => [] => 0 , ....
+
+let students: string[]= ['Kehkashan', 'Hina', 'Sana', 'Saba', 'Iqra'];
+console.log(students);
+let firstStudentName = students[0];
+console.log(firstStudentName);
+console.log(students[1] , students[2]);
+
+ 
+//For mix data type(string and numbers) in array
+
+let students_id :any = ['Iqra' , 'Huma', 10, 15];
+console.log(students_id);
+
+//For Numbers and Boolean Dta Type in An Array
+
+let feesPaid: any[]= [true , false , 1000, 1500];
+console.log(feesPaid);
+
+let fruit_Name: string[] = ['APPLE', 'MANGO', 'ORANGE'];
+console.log(fruit_Name);
+
+fruit_Name [1] = 'GRAPES';
+console.log(fruit_Name);
+
+
+
+
